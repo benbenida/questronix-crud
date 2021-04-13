361f55e169e7b19a3f29499d96731ff1a23ba7ca
benbenida <clyd.benidz@gmail.com>
2021-04-13 16:37:59 +0800

    successful conect on database using mysql

--- index.js
+++ index.js
@@ -6,8 +6,21 @@ const path = require("path");
 app.set("views", path.join(__dirname, 'views'))
 app.set("view engine", "ejs");
 
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: 'root',
+  database: 'inventory'
+})
+
+connection.connect((err) => {
+  if (err) throw err
+})
+
 app.get("/", (req, res) => {
-  res.render("index")
+  connection.query("SELECT * FROM items", function(err, rows, fields) {
+    res.render("index", {data: rows});
+  });
 })
 
 app.listen(3000, () => console.log("Serving at port 3000"))
