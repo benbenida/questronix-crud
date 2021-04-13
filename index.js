c4c88e80c55d5d2fc79df4b123d1c6e8e7f3b732
benbenida <clyd.benidz@gmail.com>
2021-04-13 17:14:33 +0800

    Added index, show, create, and update routes

--- index.js
+++ index.js
@@ -2,9 +2,13 @@ const express = require("express");
 const mysql = require("mysql")
 const app = express();
 const path = require("path");
+const methodOverride = require("method-override");
 
 app.set("views", path.join(__dirname, 'views'))
 app.set("view engine", "ejs");
+app.use(express.static("public"));
+app.use(methodOverride("_method"))
+app.use(express.urlencoded({ extended: true }));
 
 const connection = mysql.createConnection({
   host: 'localhost',
@@ -18,9 +22,42 @@ connection.connect((err) => {
 })
 
 app.get("/", (req, res) => {
+  res.redirect("/items")
+})
+
+// Index route
+app.get("/items", (req, res) => {
   connection.query("SELECT * FROM items", function(err, rows, fields) {
     res.render("index", {data: rows});
   });
+})
+
+// Show Route
+app.get("/items/:id", (req, res) => {
+  connection.query(`SELECT * FROM items WHERE item_id=${req.params.id}`, function(err, row, field) {
+    if (err) throw err;
+    res.render("show", {data: row[0]})
+  })
+  // res.render("show")
+})
+
+app.post("/items", (req, res) => {
+  const {item} = req.body;
+  connection.query(`INSERT INTO items(item_name, item_qty, item_amount) values ("${item.name}", ${item.qty}, ${item.amount});`,
+  function(err, row, field ) {
+    if (err) throw err;
+    res.redirect("/")
+  })
+});
+
+app.put("/items/:id", async (req, res) => {
+  const { item } = req.body;
+  console.log(item)
+  connection.query(`UPDATE items SET item_name = "${item.name}", item_qty = ${item.qty}, item_amount = ${item.amount} WHERE item_id=${req.params.id}`, 
+    function(err, row, field) {
+    if (err) throw err;
+    res.redirect("/")
+  })
 })
 
 app.listen(3000, () => console.log("Serving at port 3000"))
